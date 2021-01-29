eee3d258113c1e7c99716d6d13785dbaac908b69
Sam <sodonnell5214@gmail.com>
2021-01-28 22:43:31 -0500

    App is working but still needs validation and better styling

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is a simple React based Tip Calculator
 
 - After Downloading the repo, run npm install, and then start the react app as you normally would.
+
+## Oh, Hey there!
+If you are interested in getting to know me a bit better, feel free to reach out to me on [LinkedIn](https://www.linkedin.com/in/sam-o-953396119/) or my [portfolio site](https://www.novadev.cc). 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../assets/scss/main.scss';
 
 const Main = () => {
+
+  const [state, setState] = useState({
+    total: "",
+    partySize: "",
+    service: "",
+    totalTip: "",
+    tipPP: ""
+  });
+
+  const handleTotal = (event) => {
+    setState(prevState => ({
+      ...prevState,
+      total: event.target.value
+    }))
+  }
+
+  const handleService = (event) => {
+    setState(prevState => ({
+      ...prevState,
+      service: event.target.value
+    }));
+  }
+
+  const handleParty = (event) => {
+    setState(prevState => ({
+      ...prevState,
+      partySize: event.target.value
+    }))
+    console.log(state)
+
+  }
+
+  const genTip = (event) => {
+    event.preventDefault();
+
+    let tip, serviceToDecimal, totalTipPP;
+
+    serviceToDecimal = state.service / 100;
+
+    tip = parseFloat(state.total * serviceToDecimal).toFixed(2);
+    totalTipPP = parseFloat(tip / state.partySize).toFixed(2);
+
+    setState(prevState => ({
+      ...prevState,
+      totalTip: tip,
+      tipPP: totalTipPP
+    }));
+  }
+
   return (
     <>
       <div className="tipCalculatorContainer">
         <section className="tipCalculator">
-
-          <form className="calculator">
-            <label htmlFor="bill">Enter the Bill Total in order to begin.</label>
-            <input id="bill" type="text" />
-          </form>
+          <article className="calculator">
+            <form className="calculatorForm">
+              <legend>Enter the following details in order to accurately calculate your tip.</legend>
+              <div id="billDiv" className="contentDiv">
+                <label className="label" htmlFor="bill">Total:</label>
+                <input className="input" id="bill" type="text" value={state.total} onChange={handleTotal} />
+              </div>
+              <div id="serviceDiv" className="contentDiv">
+                <label className="label" htmlFor="service">Quality of Service:</label>
+                <input className="input" type="text" id="bill" value={state.service} onChange={handleService} />
+              </div>
+              <div id="sizeOfPartyDiv" className="contentDiv">
+                <label className="label" htmlFor="partySize">Size of Party:</label>
+                <input className="input" type="text" id="partySize" value={state.partySize} onChange={handleParty} />
+              </div>
+              <input className="submitBtn" type="submit" onClick={genTip} />
+            </form>
+            {
+              state.totalTip !== '' && state.tipPP !== '' ? (
+                <div className="results">
+                  <p>Tip: ${state.totalTip}</p>
+                  <p>Tip per person: ${state.tipPP}</p>
+                </div>
+              ) : ""
+            }
+          </article>
         </section>
 
       </div>
